c9cc268f82da16c8d64238853a6926207511946b
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-07-25 22:51:55 +0200

    Add multithreading to get_dictionary_entry.py

--- scripts/get_dictionary_entry.py
+++ scripts/get_dictionary_entry.py
@@ -1,4 +1,5 @@
 import requests
+import threading
 
 
 def get_page(url: str, session: requests.Session | None = None) -> str:
@@ -19,21 +20,60 @@ def split(src: str, split: str) -> (str, str):
     return (src[:start], src[start + len(split):])
 
 
-if __name__ == "__main__":
-    with open("words_urls_formated.txt", "r") as f:
-        with open("dictionary_entries.txt", "w") as g:
-            session = requests.Session()
-            session_reset = 0 # One session per 1000 requests
-            for line in f:
-                url = line.split(" ")[-1].strip()
-                print(f"GET {url}")
-                page = get_page(url, session)
-                _, page = split(page, "<!--開始 デジタル大辞泉 -->\n")
-                page, _ = split(page, "<!--終了 デジタル大辞泉-->")
-                g.write(page)
+def get_dictionary_entries(dest_path: str, urls: list[str]) -> None:
+    with open(dest_path, "w") as f:
+        session = requests.Session()
+        session_reset = 0 # One session per 1000 requests
+        for url in urls:
+            print(f"GET {url}")
+            page = get_page(url, session)
+            _, page = split(page, "<!--開始 デジタル大辞泉 -->\n")
+            page, _ = split(page, "<!--終了 デジタル大辞泉-->")
+            f.write(page)
+            session_reset += 1
+            if session_reset >= 1000:
+                session_reset = 0
+                del session
+                session = requests.Session()
 
-                session_reset += 1
-                if session_reset >= 1000:
-                    session_reset = 0
-                    del session
-                    session = requests.Session()
+
+if __name__ == "__main__":
+    THREADS_COUNT = 256
+    
+    # Count lines
+    lines = 0
+    with open("words_urls_formated.txt", "r") as f:
+        for line in f:
+            lines += 1
+
+    # Prepare input urls lists for threads
+    URLS_PER_THREAD = lines // THREADS_COUNT
+    URLS_LISTS = []
+    with open("words_urls_formated.txt", "r") as f:
+        for i in range(THREADS_COUNT):
+            URL_LIST = []
+            URLS_LISTS.append(URL_LIST)
+            for j in range(URLS_PER_THREAD):
+                line = f.readline()
+                if line == "":
+                    break
+                url = line.split(" ")[-1].strip()
+                URL_LIST.append(url)
+
+    # Start threads
+    threads = []
+    for i in range(THREADS_COUNT):
+        thr = threading.Thread(target=get_dictionary_entries,
+                               args=(f"/tmp/de_{i}", URLS_LISTS[i]))
+        threads.append(thr)
+
+    for thr in threads:
+        thr.start()
+
+    for thr in threads:
+        thr.join()
+
+    with open("dictionary_entries.txt", "w") as f:
+        for i in range(THREADS_COUNT):
+            with open(f"/tmp/de_{i}", "r") as g:
+                f.write(g.read())
